baf8359f3508acdadb8f4be1f81744b4f6c7e1a3
Ken Thompson <me@kenthompson.com.au>
2016-10-27 15:42:08 +1300

    NZ beer

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "Epic")
+option_b = os.getenv('OPTION_B', "Monteiths")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
