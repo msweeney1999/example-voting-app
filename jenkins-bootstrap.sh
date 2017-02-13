35d1b7c74ea80adc39597651039feb5ef17c8f9f
Ken Thompson - ANZ OSS <kethomps@microsoft.com>
2017-02-13 16:00:17 +1000

    Fix .kube folder permissions for tomcat user

--- jenkins-bootstrap.sh
+++ jenkins-bootstrap.sh
@@ -6,7 +6,7 @@ chmod +x kubectl
 sudo cp kubectl /usr/local/bin/kubectl
 sudo mkdir /home/tomcat/.kube
 sudo cp config /home/tomcat/.kube/config
-sudo chown tomcat:tomcat /home/tomcat/.kube/config
+sudo chown -R tomcat:tomcat /home/tomcat/.kube
 sudo usermod -aG docker tomcat
 sudo /opt/bitnami/ctlscript.sh restart
 set +x #echo off
