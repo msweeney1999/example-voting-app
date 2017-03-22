9164ea561fcddb9f9d99dd2ff3810a5b3d731ca8
Ken Thompson - ANZ OSS <kethomps@microsoft.com>
2017-03-23 10:22:43 +1300

    Nicks Pizza

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Pizza")
-option_b = os.getenv('OPTION_B', "Beer")
+option_a = os.getenv('OPTION_A', "Two")
+option_b = os.getenv('OPTION_B', "Six")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
