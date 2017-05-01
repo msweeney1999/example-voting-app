637650453f29205489e8208fa822516dfd0fe0ce
Ken Thompson - ANZ OSS <kethomps@microsoft.com>
2017-05-01 19:31:04 +1000

    Beer and Pizza changes

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Beer")
+option_b = os.getenv('OPTION_B', "Pizza")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
